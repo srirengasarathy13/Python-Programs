de8babe07724e05d59488552d5fa95670dd088e1
srirengasarathy13 <sri.rengasarathy.007@gmail.com>
2026-06-29 17:59:41 +0530

    Strings and Patterns Updated

---
+++ Patterns/square.py
@@ -0,0 +1,11 @@
+"""
+n = int(input("Enter the no. of rows: "))
+for i in range(n) :
+    for j in range(n):
+        print("*",end=" ")
+    print()
+
+"""
+n = int(input("Enter the no. of rows: "))
+for i in range(n):
+    print("* "*n)
--- Strings/ReverseSentence.py
+++ Strings/ReverseSentence.py
@@ -1,5 +1,16 @@
+"""
 sentence = "Java is awesome"
 reversed_sentence = ""
 for word in sentence.split():
     reversed_sentence = word + " " + reversed_sentence
 print("Reversed sentence:", reversed_sentence)
+"""
+str1 = 'Java is awesome'
+result = currentString =  ''
+for i in str1+' ':
+    if i == ' ':
+        result = ' '+currentString+result
+        currentString= ''
+    else:
+        currentString+=i
+print(result[1:])

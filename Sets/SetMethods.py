01d22991e199b52814d322bcabd370439ceca4b6
srirengasarathy13 <sri.rengasarathy.007@gmail.com>
2026-07-07 17:56:12 +0530

    Sets Updated

---
+++ Sets/SetMethods.py
@@ -0,0 +1,16 @@
+stream1 = {1, 3, 4}
+stream2 = {2, 3, 5}
+
+result = stream1.union(stream2)
+print(result)
+
+# result = stream1.difference(stream2)
+# result = stream1.intersection(stream2)
+# result = stream1.copy()
+# stream1.add(6)
+# stream1.update([6, 7, 9]) 
+# stream1.difference_update(stream2)
+# stream1.intersection_update(stream2)
+#print(stream1.pop())
+#stream1.discard(3)
+

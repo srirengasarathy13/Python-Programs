4a3b5ee2c7222608441e7563fcef448fcc103e4f
srirengasarathy13 <sri.rengasarathy.007@gmail.com>
2026-07-09 17:54:57 +0530

    Patterns with Functions Updated

--- Patterns/HollowRightInvertedTriangle.py
+++ Patterns/HollowRightInvertedTriangle.py
@@ -1,7 +1,12 @@
-# Program to print the right triangle pattern
-n = int(input("Enter the no. of rows : "))
-for i in range(n, 0, -1):
-    if(i == 1 or i == n):
-        print("  "*(n - i) + "* " * i)
-    else:
-        print("  "*(n - i) + "* " + "  " * (i - 2) + "* ")
+# Program to print the right triangle pattern with return type
+def right_inverted_triangle(n):
+    result = ""
+    for i in range(n, 0, -1):
+        if(i == 1 or i == n):
+            result += "  "*(n - i) + "* " * i + "\n"
+        else:
+            result += "  "*(n - i) + "* " + "  " * (i - 2) + "* \n"
+    return result.strip()
+            
+n=int(input("Enter the number of rows: "))
+print(right_inverted_triangle(n))

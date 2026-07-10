56d6e9060b16e4fc360749f3ec6f86cb9a9d995d
srirengasarathy13 <sri.rengasarathy.007@gmail.com>
2026-07-10 17:59:02 +0530

    F Strings and Format() Updated

---
+++ format.py
@@ -0,0 +1,20 @@
+name = "Sri"
+age = 22
+print("My name is {name} and I am {age} years old.".format(name="Sri", age=22))
+print(f"My name is {name} and I am {age} years old.")
+
+#-------------------------------------------------------------------------------------
+
+print(f"{49/ 7}")
+
+print(f"{10:b}")
+
+print(f"{10:4d}")
+
+print(f"{10:3}")
+
+print(f"{10:>05}")
+
+print(f"{10:<05}")
+
+print(f"{10:^05}")

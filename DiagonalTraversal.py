9055298a74f768ed2b9a27286e3df604b098d598
srirengasarathy13 <sri.rengasarathy.007@gmail.com>
2026-07-21 14:11:28 +0530

    Diagonal Traversal Updated

--- DiagonalTraversal.py
+++ DiagonalTraversal.py
@@ -7,7 +7,7 @@ for d in range(rows + cols - 1):
     for i in range(rows):
         j = d - i
         if 0 <= j < cols:
-            temp += words[i][j]
+            temp += words[i][j] 
     result.append(temp)
 for i in range(len(result)):
     if i%2!=0:

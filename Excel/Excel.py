414950e75ee0dfd8b8cc93e583843aa8d997ee90
srirengasarathy13 <sri.rengasarathy.007@gmail.com>
2026-07-28 17:59:52 +0530

    Excel using Python Updated

--- Excel/Excel.py
+++ Excel/Excel.py
@@ -1,36 +1,58 @@
 from openpyxl import *
 from openpyxl.utils import get_column_letter
 from openpyxl.worksheet.table import Table,TableStyleInfo
-import os
+import os 
+
 path = r'Excel\Data.xlsx'
-workbook = Workbook()
 if  os.path.exists(path):
     print("Path already exist !")
     workbook = load_workbook(path)
-else:
-    workbook[workbook.sheetnames[0]].title = 'Data1'
     worksheet = workbook.active
-    table = Table(displayName = 'Employee_Table', ref = 'A1:D1')
-    table.tableStyleInfo = TableStyleInfo(name = 'TableStyleMedium2', showFirstColumn=False, showLastColumn=False, showColumnStripes=True, showRowStripes=True )
+    table = worksheet.tables['Team_Agnie']
+else:
+    workbook = Workbook()
+    workbook[workbook.sheetnames[0]].title = 'Boys'
+    worksheet = workbook.active
+    table = Table(displayName = 'Team_Agnie')
+    table.tableStyleInfo = TableStyleInfo(name = 'TableStyleLight12', showFirstColumn=False, showLastColumn=False, showColumnStripes=True, showRowStripes=True )
     worksheet.add_table(table)
+    table.ref = f'A1:{get_column_letter(worksheet.max_column)}{worksheet.max_row}'
 
 # worksheet['A1'] = 'Sri'
 # worksheet['A2'] = 'Renga'
 # worksheet['A3'] = 'Sarathy'
-# worksheet.append(['R','Sri','Rengasarathy'])
-# worksheet.append(['R','Yuvaraj','Kannan'])
-# worksheet.append(['R','Naveen','Raja'])
-# worksheet.append(['P S','Kiruba','Nidhi'])
-# worksheet.append(['Y','Nikish','Daniel'])
-# worksheet.append(['J S','Prawin','Kumar'])
-# worksheet.append(['M','Sethu','Raj'])
-# table.ref = f'A1:{get_column_letter(worksheet.max_column)}{worksheet.max_row}'
-# workbook.create_sheet('Data2')
+worksheet.append(['Intial','FirstName','LastName'])
+worksheet.append(['R','Sri','Rengasarathy'])
+worksheet.append(['R','Yuvaraj','Kannan'])
+worksheet.append(['R','Naveen','Raja'])
+worksheet.append(['P S','Kiruba','Nidhi'])
+worksheet.append(['Y','Nikish','Daniel'])
+worksheet.append(['J S','Prawin','Kumar'])
+worksheet.append(['M','Sethu','Raj'])
+table.ref = f'A1:{get_column_letter(worksheet.max_column)}{worksheet.max_row}'
+print("Max Row : ",worksheet.max_column)
+print("Max Column : ",worksheet.max_row)
+print(f'A1:{get_column_letter(worksheet.max_column)}{worksheet.max_row}')
+
+workbook.create_sheet('Girls')
+
 # worksheet.delete_rows(1,3)
 # worksheet.delete_rows(1)
 # worksheet.delete_cols(1,3)
 # worksheet.delete_cols(1)
 
+print(workbook.sheetnames)
+
+worksheet = workbook['Girls']
+worksheet.append(['Intial','Name'])
+worksheet.append(['G R','Harini'])
+worksheet.append(['A','Prithika'])
+
+workbook.active = workbook.index(worksheet)
+
+
+# workbook[workbook.sheetnames[0]].title = 'BOYS'
+# worksheet.title = 'GIRLS'
 
 # if 'Employee_Table' not in workbook.tables:
 #     workbook.append(['Id','Name','Age','Languages Known'])
@@ -38,5 +60,6 @@ else:
 # else:
 #     table = workbook.tables['Employee_Table']
 
-
+print(workbook.active)
 workbook.save(path)
+workbook.close()

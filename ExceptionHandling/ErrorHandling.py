a8362ff6d0cc5ed08ca86cb019e27853bf7f7892
srirengasarathy13 <sri.rengasarathy.007@gmail.com>
2026-08-11 17:55:02 +0530

    Updated

---
+++ ExceptionHandling/ErrorHandling.py
@@ -0,0 +1,15 @@
+try:
+    number1, number2 = int(input()), int(input())
+    result = number1 / number2
+
+except ValueError:
+    print("Please enter numbers only.")
+
+except ZeroDivisionError:
+    print("Cannot divide by zero.")
+
+else:
+    print("Result:", result)
+
+finally:
+    print("Program Ended")
